c5165057d4e5bc498081a5491469cefa3e72eb32
Christlieb Dela <christliebdela@gmail.com>
2026-09-03 04:49:32 +0000

    chore: release v0.4.0

--- src/store/useUpdaterStore.ts
+++ src/store/useUpdaterStore.ts
@@ -44,7 +44,7 @@ const getStoredLastChecked = (): string | null => {
 
 export const useUpdaterStore = create<UpdaterState>((set, get) => ({
   status: 'idle',
-  currentVersion: '0.3.0',
+  currentVersion: '0.4.0',
   availableVersion: null,
   releaseDate: null,
   releaseNotes: null,
